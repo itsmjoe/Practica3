4878a05ac251de211f0d7be477588c8567baeef3
Mynor Joel Molina <mynormolina1@gmail.com>
2022-02-17 22:26:26 -0600

    Cambio corregido

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,6 +19,6 @@ describe('AppComponent', () => {
   it(`should have as title 'frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('frontend1');
+    expect(app.title).toEqual('frontend');
   });
 });
